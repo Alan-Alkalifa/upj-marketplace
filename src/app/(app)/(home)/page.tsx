a25e23f29c7286bb059b107c34b51719214db2dc
Alan-Alkalifa <work.alanalkalifa@gmail.com>
2025-08-15 19:26:09 +0700

    04: Payload Intergration

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,44 +1,18 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectItem,
-} from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  })
+  
+  const data = await payload.find ({
+    collection: "users",
+
+  })
   return (
-    <div className="p-4">
-      <div className="flex flex-col gap-y-4">
-        <div>
-          <Button>I am a button</Button>
-        </div>
-        <div>
-          <Input placeholder="Enter your name" />
-        </div>
-        <div>
-          <Progress value={50} />
-        </div>
-        <div>
-          <Textarea value="Enter your message" />
-        </div>
-        <div>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Select an option" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="1">Option 1</SelectItem>
-              <SelectItem value="2">Option 2</SelectItem>
-              <SelectItem value="3">Option 3</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
+      <div>
+        {JSON.stringify(data, null, 2)}
       </div>
-    </div>
   );
 }
